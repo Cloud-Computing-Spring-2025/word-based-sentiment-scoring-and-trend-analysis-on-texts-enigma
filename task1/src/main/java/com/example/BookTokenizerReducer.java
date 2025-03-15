ec8dea31407f81f41e16ca6f6dfbc20a7fae1455
Hayden <66452827+HaydenCJuneau@users.noreply.github.com>
2025-03-15 12:45:01 -0400

    Finished Reducer

--- task1/src/main/java/com/example/BookTokenizerReducer.java
+++ task1/src/main/java/com/example/BookTokenizerReducer.java
@@ -2,38 +2,22 @@ package com.example;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class BookTokenizerReducer extends Reducer<BookWritable, Text, BookWritable, Text> {
 
-    private BookWritable document = new BookWritable();
-    private final static IntWritable one = new IntWritable(1);
+    private BookWritable bookDetails = new BookWritable();
+    private Text fullText = new Text();
 
     @Override
-    protected void reduce(Text key, Iterable<BookWritable> values, Context context) throws IOException, InterruptedException {
-        List<BookWritable> sorted = new ArrayList<>();
-        for (BookWritable d: values) sorted.add(d);
+    protected void reduce(BookWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        ArrayList<String> textLines = new ArrayList<String>();
+        for (Text t: values) textLines.add(t.toString());
 
-        Collections.sort(sorted);
+        fullText.set(String.join(" ", textLines));
 
-        // Make Pairs
-        for (int i = 0; i < sorted.size() - 1; i++) {
-            BookWritable a = sorted.get(i);
-
-            for (int j = i + 1; j < sorted.size(); j++) {
-                BookWritable b = sorted.get(j);
-
-                document.setTitle(a.getTitle() + "_" + b.getTitle());
-                document.setYear(a.getYear() + b.getYear());
-
-                context.write(document, one);
-            }
-        }
+        context.write(bookDetails, fullText);
     }
-        
 }
