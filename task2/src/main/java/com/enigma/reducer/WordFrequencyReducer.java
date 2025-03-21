2dd810d23bf64eb9eb7de5242ddfae2b8e11bdd5
Shreyash Thakre <sthakre@uncc.edu>
2025-03-21 02:42:47 +0000

    fixed output format

--- task2/src/main/java/com/enigma/reducer/WordFrequencyReducer.java
+++ task2/src/main/java/com/enigma/reducer/WordFrequencyReducer.java
@@ -5,21 +5,22 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
-public class WordFrequencyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-    private IntWritable result = new IntWritable();
+public class WordFrequencyReducer extends Reducer<Text, IntWritable, Text, Text> {  // Changed output value type to Text
 
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int sum = 0;
-        
+
         // Sum all occurrences of the lemma for the (bookID, lemma, year) key
         for (IntWritable val : values) {
             sum += val.get();
         }
+
+        // Output: bookID,lemma,year,frequency as a single Text value
+        String keyStr = key.toString();
+        String valueStr = Integer.toString(sum);
         
-        // Output: (bookID, lemma, year) => frequency
-        result.set(sum);
-        context.write(key, result);
+        // Output combined format
+        context.write(new Text(keyStr + "," + valueStr), new Text(""));  // setting empty Text as value
     }
 }
