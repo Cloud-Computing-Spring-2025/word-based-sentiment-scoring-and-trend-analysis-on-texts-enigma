26b1bdf3f8d542964dc285426b6210b0c2992243
VinayD2028 <vdevabh1@charlotte.edu>
2025-03-24 14:49:50 +0000

    task5_final_without_readme

--- task5/BigramUDF.java
+++ task5/BigramUDF.java
@@ -1,16 +1,18 @@
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
 public class BigramUDF extends UDF {
-    public Map<String,Integer> evaluate(List<String> words) {
-        Map<String,Integer> bigramCounts = new HashMap<>();
+    public Map<String,DoubleWritable> evaluate(List<String> words, List<Integer> counts) {
+        Map<String,DoubleWritable> bigramCounts = new HashMap<>();
         if (words == null || words.size() < 2) return bigramCounts;
         
         for(int i=0; i<words.size()-1; i++) {
-            String bigram = words.get(i) + " " + words.get(i+1);
-            bigramCounts.put(bigram, bigramCounts.getOrDefault(bigram, 0) + 1);
+            String bigram = words.get(i) + "_" + words.get(i+1);
+            int minCount = Math.min(counts.get(i), counts.get(i+1));
+            bigramCounts.put(bigram, new DoubleWritable(minCount));
         }
         return bigramCounts;
     }
